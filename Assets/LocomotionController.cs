78dec3ecfe35cfecc58c7443c6552a35a4f8c2ca
PatrickDarcy <C00226157@itcarlow.ie>
2020-11-25 01:57:53 +0000

    Added Player Teleportation

---
+++ Assets/LocomotionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController LeftTeleportRay;
+    public XRController RightTeleportRay;
+    public InputHelpers.Button teleportrActivationButton;
+    public float activationThreshold = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LeftTeleportRay)
+        {
+            LeftTeleportRay.gameObject.SetActive(CheckIfActivated(LeftTeleportRay));
+        }        
+        if(RightTeleportRay)
+        {
+            RightTeleportRay.gameObject.SetActive(CheckIfActivated(RightTeleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportrActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+}
