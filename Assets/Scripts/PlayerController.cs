f5b84a407fc50295e063d0f64cb99d5a98770f22
PatrickDarcy <C00226157@itcarlow.ie>
2021-04-30 13:14:20 +0100

    AI finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float m_playerHealth;
+    public GameObject m_moverRay;
+    public XRNode m_inputSource;
+
+    private bool m_aButtonPressed;
+    private bool m_canShoot;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,5 +31,32 @@ public class PlayerController : MonoBehaviour
     public void TakeDamage()
     {
         m_playerHealth--;
+    }
+
+    private void WeaponChange()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(m_inputSource);
+        device.TryGetFeatureValue(CommonUsages.primaryTouch, out m_aButtonPressed);
+        if (!m_canShoot)
+        {
+            if (m_aButtonPressed)
+            {
+                m_canShoot = true;
+                gameObject.transform.GetChild(0).transform.GetChild(3).gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            if (m_aButtonPressed)
+            {
+                m_canShoot = false;
+                gameObject.transform.GetChild(0).transform.GetChild(3).gameObject.SetActive(true);
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+
     }
 }
